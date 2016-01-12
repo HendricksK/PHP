eb3275abf80c7bc090037c840b80e76fdc3803ef
HendricksK <kurvinuniversity@gmail.com>
2016-01-12 15:55:28 +0200

    added a quick and archaich menu, think i will need to add a switch statement to reduce the database class which i will get into tomorrow

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<?php include('header.php');?>
+<body>
+	
+<ul>
+  <a href="order.php"><li>Orders</li></a>
+  <a href="customer.php"><li>Customer</li></a>
+  <a href="dvd.php"><li>DVDs</li></a>
+</ul>
+
+</body>
+</html>
