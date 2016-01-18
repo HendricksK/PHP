69aba3af747640a718e29bd478ea984b8c5c9f13
Kurvin Hendricks <kurvinuniversity@gmail.com>
2016-01-18 08:09:14 +0200

    fixing up the echo's as to reduce the load on PHP itself

--- customer.php
+++ customer.php
@@ -10,63 +10,52 @@
 	<td>SA ID Number</td>
 	<td>Address</td>
 	<td>Actions</td></tr>
-
 <?php 
 $dbconn = new mysql_database();
 
 $result = $dbconn->fetch("select * from customer");
 
 if ($result->num_rows > 0) {
-    // output data of each row
+    while($row = $result->fetch_assoc()) { ?>
+        <tr><td><?php echo $row["ID"];?></td> 
+        <td><?php echo $row["name"];?></td> 
+        <td><?php echo $row["surname"];?></td> 
+        <td><?php echo $row["contact_number"];?></td> 
+        <td><?php echo $row["email"];?></td>
+        <td><?php echo $row["sa_id_number"];?></td>
+        <td><?php echo $row["address"];?>
+        <td><form action="customer.php?customer_ID=<?php echo $row["ID"];?>" method="post"><input type="submit" value="edit"></form></td>
+        <td><form action="delete_customer.php?ID=<?php echo $row["ID"];?>" method="post"><input type="submit" value="delete"></form></td>
+    <?php }
+} ?>
 
-    while($row = $result->fetch_assoc()) {
-        echo "<tr>"."<td>" . $row["ID"]."</td>". 
-        " <td>" . $row["name"]. "</td>". 
-        " <td>" . $row["surname"]. "</td>". 
-        "<td>" . $row["contact_number"] ."</td>" . 
-        "<td>". $row["email"] ."</td>".
-        "<td>". $row["sa_id_number"] .
-        "<td>". $row["address"] .
-        "<td><form action='customer.php?q=".$row["ID"]."' method='post'><input type='submit' value='edit'></form></td>" .
-        "<td><form action='delete_customer.php?ID=".$row["ID"]."' method='post'><input type='submit' value='delete'></form></td>";
-    }
-}
-  
-?>
-
-</td></tr>
-</table>
+</tr></table>
 
 <?php
 
-if (isset($_GET["q"]))
+if (isset($_GET["customer_ID"]))
 {
-   $id = ($_GET["q"]);
+   $id = ($_GET["customer_ID"]);
 
    $result = $dbconn->fetch("select * from customer where ID=".$id."");
    
    if ($result->num_rows > 0) {
 
    		while($row = $result->fetch_assoc()) {
-       		
-       		echo '<form method="post" action="edit_customer.php?ID='.$row["ID"].'">
-					Name: <input type="text" name="name" value="'.$row["name"] .'" required><br>
-					Surname: <input type="text" name="surname" value="'.$row["surname"] .'" required><br>
-					Contact: <input type="text" name="contact_number" value="'.$row["contact_number"].'" required><br>
-					E-mail: <input type="text" name="email" value="'.$row["email"].'" required><br>
-					RSA ID: <input type="text" name="sa_id_number" value="'.$row["sa_id_number"].'" required><br>
-					Address: <input type="text" name="address" value="'.$row['address'].'" required><br>
-					<input type="submit">
-					</form>';
+       		?>
+       		<form method="post" action="edit_customer.php?ID=<?php echo $row["ID"];?>">
+                    Name: <input type="text" name="name" value="<?php echo $row["name"];?>" required><br>
+                    Surname: <input type="text" name="surname" value="<?php echo $row["surname"];?>" required><br>
+                    Contact: <input type="text" name="contact_number" value="<?php echo $row["contact_number"];?>" required><br>
+                    E-mail: <input type="text" name="email" value="<?php echo $row["email"];?>" required><br>
+                    RSA ID: <input type="text" name="sa_id_number" value="<?php echo $row["sa_id_number"];?>" required><br>
+                    Address: <input type="text" name="address" value="<?php echo $row["address"];?>" required><br>
+                    <input type="submit">
+		</form> <?php
     	}
    }
-}
-else
-{
-   //echo "<p>not clicked";
-}
-?>
+} ?>
 
-<form action="add_customer.php" method="post"><input type="submit" value="Add Customer"></form>
-</body>
+        <form action="add_customer.php" method="post"><input type="submit" value="Add Customer"></form>
+    </body>
 </html>
